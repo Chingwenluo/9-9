22db356b7ae773db88b9eb568ecadc9256c70749
ChingWen <fsvnxwx@gmail.com>
2020-12-10 23:39:27 +0800

    Initial Commit

---
+++ 9*9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9*9
+//
+//  Created by eric on 2020/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
