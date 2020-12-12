4f627aa9f44375304e1b741e5957df57e6c2d8e4
ChingWen <fsvnxwx@gmail.com>
2020-12-12 18:22:02 +0800

    update

--- 9*9/ViewController.swift
+++ 9*9/ViewController.swift
@@ -9,11 +9,111 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    @IBOutlet weak var shownumber: UILabel!
+    @IBAction func count(_ sender: UISlider) {
+       
+      
+        
+        sender.value = sender.value.rounded()
+        let number = Int(sender.value)
+        shownumber.text = "\(number)乘\(number)"
+        let width = view.frame.width / 10
+
+        var x: CGFloat = 5
+           for Horizontal in 0...number {
+        var y: CGFloat = 90
+              for straight in 0...number {
+                //產生多個label
+                  let label = UILabel(frame: CGRect(x: x, y: y, width: 30 , height: 30))
+                
+                     if Horizontal == 0 , straight == 0 {
+                        label.text = "X"
+                        label.font = UIFont.boldSystemFont(ofSize: 17) //文字粗體
+                        label.textAlignment = .center //文字置中
+                     }else if  Horizontal != 0, straight == 0 {
+                        label.text = "\(Horizontal)"
+                        label.font = UIFont.boldSystemFont(ofSize: 17) //文字粗體
+                        label.textAlignment = .center //文字置中
+                     }else if Horizontal == 0, straight != 0 {
+                        label.text = "\(straight)"
+                        label.font = UIFont.boldSystemFont(ofSize: 17) //文字粗體
+                        label.textAlignment = .center //文字置中
+                     }else{
+                        label.text = "\(straight*Horizontal)"
+                        label.font = UIFont.systemFont(ofSize: 17)
+                        label.textAlignment = .center //文字置中
+                     }
+                
+                if  (Horizontal > 1 && Horizontal < 5 && Horizontal+straight == 10) || (Horizontal >= 5 && Horizontal == straight && Horizontal != 9)  {
+                    label.backgroundColor = UIColor.systemPink
+                } else if straight == 9 ,Horizontal % 2 != 0 {
+                    label.backgroundColor = UIColor.orange
+                } else if straight == 9 , Horizontal != 0 , Horizontal % 2 != 1{
+                    label.backgroundColor = UIColor.systemBlue
+                }
+
+                
+                
+                             view.addSubview(label)
+                     y = y + width
+                  }
+                     x = x + width
+             }
+        
+    }
+    
+    override func viewDidLoad() {
+  
+        super.viewDidLoad()
+  
+        //title設粗體
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 20)]
+        
+//        let width = view.frame.width / 10
+//
+//        var x: CGFloat = 5
+//           for Horizontal in 0...9 {
+//        var y: CGFloat = 90
+//              for straight in 0...9 {
+//                //產生多個label
+//                  let label = UILabel(frame: CGRect(x: x, y: y, width: 30 , height: 30))
+//
+//                     if Horizontal == 0 , straight == 0 {
+//                        label.text = "X"
+//                        label.font = UIFont.boldSystemFont(ofSize: 17) //文字粗體
+//                        label.textAlignment = .center //文字置中
+//                     }else if  Horizontal != 0, straight == 0 {
+//                        label.text = "\(Horizontal)"
+//                        label.font = UIFont.boldSystemFont(ofSize: 17) //文字粗體
+//                        label.textAlignment = .center //文字置中
+//                     }else if Horizontal == 0, straight != 0 {
+//                        label.text = "\(straight)"
+//                        label.font = UIFont.boldSystemFont(ofSize: 17) //文字粗體
+//                        label.textAlignment = .center //文字置中
+//                     }else{
+//                        label.text = "\(straight*Horizontal)"
+//                        label.font = UIFont.systemFont(ofSize: 17)
+//                        label.textAlignment = .center //文字置中
+//                     }
+//
+//                if  (Horizontal > 1 && Horizontal < 5 && Horizontal+straight == 10) || (Horizontal >= 5 && Horizontal == straight && Horizontal != 9)  {
+//                    label.backgroundColor = UIColor.systemPink
+//                } else if straight == 9 ,Horizontal % 2 != 0 {
+//                    label.backgroundColor = UIColor.orange
+//                } else if straight == 9 , Horizontal != 0 , Horizontal % 2 != 1{
+//                    label.backgroundColor = UIColor.systemBlue
+//                }
+//
+//
+//
+//                             view.addSubview(label)
+//                     y = y + width
+//                  }
+//                     x = x + width
+//             }
+        }// Do any additional setup after loading the view.
+        
 
 }
 
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+
+for i in 1...9 {
+    for j in 1...9 {
+        let sum = i * j
+   print("\(i)*\(j)=\(sum)")
+    }
+}
